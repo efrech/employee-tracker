f9b0cfb2eecac4a3543273e5f1327d0e8117896a
Evelyn Frech <evelynmariafrech@gmail.com>
2022-05-02 23:37:17 -0400

    Classes Employee, Department and Role bugs modified. inquirer updated to a more recent version. server.js prompts modified

--- server.js
+++ server.js
@@ -21,59 +21,33 @@ afterConnection = () => {
 };
 
 // inquirer prompt for first action
-function promptUser() {
-  inquirer.prompt ([
+const promptUser = () => {
+  inquirer.prompt([
     {
       type: 'list',
-      name: 'choices', 
+      name: 'choices',
       message: 'What would you like to do?',
-      choices: ['View all departments', 
-                'View all roles', 
-                'View all employees', 
-                'Add a department', 
-                'Add a role', 
-                'Add an employee', 
-                'Update an employee role',
-                // 'Update an employee manager',
-                // "View employees by department",
-                // 'Delete a department',
-                // 'Delete a role',
-                // 'Delete an employee',
-                // 'View department budgets',
-                // 'No Action'
-              ]
+      choices: ['View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ]
     }
   ])
     .then((answers) => {
-      const { choices } = answers; 
+      const { choices } = answers;
       if (choices === "View all departments") {
-        let department = new Department;
-        console.table(department.getAll(function(err, data) {
-          if(err) {
-             // handle the error
-             console.log(err)
-          } else {
-          // handle data
-            console.table(data)
-          }
-       }
-       ));
+        getAll(Department);
       }
       if (choices === "View all roles") {
-        let role = new Role;
-        console.table(role.getAll(function(err, data) {
-          if(err) {
-             // handle the error
-             console.log(err)
-          } else {
-          // handle data
-            console.table(data)
-          }
-       }));
+        getAll(Role);
       }
       if (choices === "View all employees") {
-        let employee = new Employee;
-        employee.getAll();
+        getAll(Employee);
       }
       if (choices === "Add a department") {
         addDepartmentPrompt();
@@ -81,80 +55,212 @@ function promptUser() {
       if (choices === "Add a role") {
         addRolePrompt();
       }
-      // if (choices === "Add an employee") {
-      //   addEmployeePrompt();
-      // }
-      // if (choices === "Update an employee role") {
-      //   updateEmployeePrompt();
-      // }
-
-    });
-  }
-
-  //Function to ask questions to add a department
-  addDepartmentPrompt = () => {
-    inquirer.prompt ([
-      {
-        type: 'input',
-        name: 'newDepartment',
-        message: "what is the name for the new department?"
+      if (choices === "Add an employee") {
+        addEmployeePrompt();
       }
-    ])
+      if (choices === "Update an employee role") {
+        updateEmployeePrompt();
+      }
+      if (choices === "Exit") {
+        process.exit();
+      }
+    });
+}
+
+
+//Function to view all departments
+getAll = (klassParam) => {
+  let klass = new klassParam;
+  klass.getAll(function (err, data) {
+    if (err) {
+      // handle the error
+      console.log(err)
+    } else {
+      // handle data
+      console.log(`\n`);
+      console.table(data)
+      console.log(`\n`);
+      promptUser();
+    }
+  }
+  );
+}
+
+//Function to ask questions to add a department
+addDepartmentPrompt = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'newDepartment',
+      message: "what is the name for the new department?"
+    }
+  ])
     .then(answer => {
       let department = new Department;
       department.addDepartment(answer.newDepartment);
+      console.log(`\n`);
+      console.log(chalk.bgGray(`\n${answer.newDepartment} added to the database`));
+      console.log(`\n`);
+      promptUser();
     })
-  }
+}
 
 
-  //Function inquirer to add a new role
-  addRolePrompt = () => {
-    var allDepartments = [];
-    let department = new Department;
-    var rows = [];
-    department.getAll(function(err, data) {
-      if(err) {
-         // handle the error
-         console.log(err)
-      } else {
+//Function inquirer to add a new role
+addRolePrompt = () => {
+  var allDepartments = [];
+  let department = new Department;
+  department.getAll(function (err, data) {
+    if (err) {
+      // handle the error
+      console.log(err)
+    } else {
       // handle data
-        data.forEach(row => allDepartments.push({name: row.name, value: row.id}));
-
-        inquirer.prompt ([
-          {
-            type: 'input',
-            name: 'newTitle',
-            message: "what is the title for the new role?"
-          },
-          {
-            type: 'input',
-            name: 'newSalary',
-            message: "what is the salary for the new role?"
-          },
-          {
-            type: 'list',
-            name: 'newDepartmentId',
-            message: "Assign a department id",
-            choices: allDepartments
-          }
-        ])
+      data.forEach(row => allDepartments.push({ name: row.name, value: row.id }));
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'newTitle',
+          message: "what is the title for the new role?"
+        },
+        {
+          type: 'input',
+          name: 'newSalary',
+          message: "what is the salary for the new role?"
+        },
+        {
+          type: 'list',
+          name: 'newDepartmentId',
+          message: "Assign a department id",
+          choices: allDepartments
+        }
+      ])
         .then(answer => {
           let role = new Role;
-          role.addRole(answer.newTitle, answer.newSalary, answer.newDepartmentId, function(err, data) {
-            if(err) {
-               // handle the error
-               console.log(err)
+          role.addRole(answer.newTitle, answer.newSalary, answer.newDepartmentId);
+          console.log(`\n`);
+          console.log(chalk.bgGray(`\n${answer.newTitle} added to the database`));
+          console.log(`\n`);
+          promptUser();
+        })
+    }
+  })
+}
+
+addEmployeePrompt = () => {
+  var allRoles = [];
+  let role = new Role;
+  let employee = new Employee;
+  role.getAll(function (err, data) {
+    if (err) {
+      // handle the error
+      console.log(err)
+    } else {
+      // handle data
+      data.forEach(row => allRoles.push({ name: row.title, value: row.id }));
+      // first_name, last_name, role_id, manager_id
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'newFirstName',
+          message: "what is the new employee first name?"
+        },
+        {
+          type: 'input',
+          name: 'newLastName',
+          message: "what is the new employee last name?"
+        },
+        {
+          type: 'list',
+          name: 'newRoleId',
+          message: "Assign a role id",
+          choices: allRoles
+        }
+      ])
+        .then(answer => {
+          const newEmployee = { first_name: answer.newFirstName, last_name: answer.newLastName, role_id: answer.newRoleId };
+
+          employee.getManagers(function (err, data) {
+            if (err) {
+              // handle the error
+              console.log(err)
             } else {
               // handle data
-              console.log(data)
+              // first_name, last_name, role_id, manager_id
+              data.unshift({value: 0, name: "None"});
+              inquirer.prompt([
+                {
+                  type: 'list',
+                  name: 'newManagerId',
+                  message: "Assign a manager id",
+                  choices: data
+                }
+              ])
+                .then(answer => {
+                  newEmployee.manager_id = answer.newManagerId;
+                  employee.addEmployee(newEmployee.first_name, newEmployee.last_name, newEmployee.role_id, newEmployee.manager_id);
+                  console.log(`\n`);
+                  console.log(chalk.bgGray(`\nNew employee added to the database`));
+                  console.log(`\n`);
+                  promptUser();
+                })
             }
-         });
+          })
         })
-      }
-   })
-  }
+   }})
+}
 
-  //
+updateEmployeePrompt = () => {
+  var allEmployees = [];
+  var allRoles = [];
+  let role = new Role;
+  let employee = new Employee;
+  employee.getAll(function (err, data) {
+    if (err) {
+      // handle the error
+      console.log(err)
+    } else {
+      // handle data
+      data.forEach(row => allEmployees.push({ name: row.first_name + " " + row.last_name, value: row.id }));
+      // first_name, last_name, role_id, manager_id
+      inquirer.prompt([
+        {
+          type: 'list',
+          name: 'employeeId',
+          message: "Which employee's role do you want to update?",
+          choices: allEmployees
+        }
+      ])
+        .then(answer => {
+          const employeeId = answer.employeeId;
+
+          role.getAll(function (err, data) {
+            if (err) {
+              // handle the error
+              console.log(err)
+            } else {
+              // handle data
+              // first_name, last_name, role_id, manager_id
+              data.forEach(row => allRoles.push({ name: row.title, value: row.id }));
+              inquirer.prompt([
+                {
+                  type: 'list',
+                  name: 'newRoleId',
+                  message: "Which role do you want to assign the selected employee?",
+                  choices: allRoles
+                }
+              ])
+                .then(answer => {
+                  employee.updateEmployeeRole(employeeId, answer.newRoleId);
+                  promptUser();
+                })
+            }
+          })
+        })
+   }})
+}
+
+//
 
 //Classes
 // let employee = new Employee;
@@ -175,14 +281,14 @@ function promptUser() {
 
 
 //TODO: create class per table
-  //the class should have functions for queries: update an employee role
-  //- create index.js and innit() in server.js
-  //https://stackoverflow.com/questions/46210279/pass-objects-in-array-into-inquirer-list-choices - inquirer list name value pair
+//the class should have functions for queries: update an employee role
+//- create index.js and innit() in server.js
+//https://stackoverflow.com/questions/46210279/pass-objects-in-array-into-inquirer-list-choices - inquirer list name value pair
 
-  // var departments = [];
-  // for loop departments
-  // var d = { name: departnemt.name, value: department.id }
-  // departments.push(d)
-  // list: departments
+// var departments = [];
+// for loop departments
+// var d = { name: departnemt.name, value: department.id }
+// departments.push(d)
+// list: departments
 
-  afterConnection();
+afterConnection();
